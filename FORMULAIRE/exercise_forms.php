51cf9d229640ec3fbcf499b980835d2f32c7d7b0
TOF075 <xtophePERES@gmail.com>
2019-01-30 16:58:22 +0100

    Ajout d'éléments concernant le formulaire - non définitif

---
+++ FORMULAIRE/exercise_forms.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>     
+<html>     
+    <head>         
+        <title>Formulaire HTML</title>     
+    </head>     
+    <body> 
+        <form name="form1" method="" action=""> 
+ <input type="text"  value="Prenom">  <input type="submit"  name="submit1"   value="login">
+ <input type="text"  value="Nom">  <input type="submit"  name="submit1"   value="login"> 
+        </form> 
+
+        <?php
+
+        echo "klkljkljhkl";
+        ?>
+    </body>     
+</html>
